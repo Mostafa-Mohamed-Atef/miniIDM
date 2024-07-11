32c04bcb29ad47cee45631ed69969825a272b89f
Mostafa-Mohamed-Atef <mostafamohamedatef27@gmail.com>
2024-07-11 19:20:59 +0300

    still on working

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import tkinter 
-from pytube import YouTube
+from pytube import *
 
 def Download(url):
     try:
-        YouTube.streams
+       yt = YouTube(url).streams.first().download()
+       print(yt.title)
+       print(yt.streams.filter(file_extension='mp4'))
+    except:
+        print("didn't work")
 
-url = input()
+url = input("import your link: \n")
 Download(url)
