386e9c76e117c577fc75b80e79b1728476a2f886
Mostafa-Mohamed-Atef <mostafamohamedatef27@gmail.com>
2024-08-05 18:27:57 +0300

    added progress bar

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
-import tkinter 
-import ttkbootstrap as ttk 
 from pytubefix import YouTube
+from tqdm import tqdm
 
-def Download(url):
+progress_bar = None
+def download(url):
+    global progress_bar
     try:
-       yt = YouTube(url)
        yd = yt.streams.get_highest_resolution()
        print(yt.title)
+       progress_bar = tqdm(total=yd.filesize, unit='0', unit_scale=True, desc="Progress")
        yd.download("/home/mostafa/programming/python/miniIDM/")
+       progress_bar.close()
        print('completed')
     except Exception as e:
         print(e)
+def progress(stream, chunk, bytes_remaining):
+    global progress_bar
+    file_size = stream.filesize
+    bytes_downloaded = file_size - bytes_remaining
+    progress_bar.update(len(chunk))
 
 url = input("import your link: \n")
-Download(url)
+yt = YouTube(url, on_progress_callback=progress)
+download(url)
